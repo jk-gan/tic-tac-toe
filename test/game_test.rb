ac3a1781f9b32b6601298b8e95fb7c2dccd71ed1
Gan Jun Kai <kuhn96@gmail.com>
2017-05-28 19:03:42 +0800

    Remove unused code

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,14 +1,4 @@
 require "test_helper"
 
 class GameTest < Minitest::Test
-  def setup
-    @board = TicTacToe::Board.new
-    @players = []
-    @players << TicTacToe::Player.new('JK', 'X', role[:human])
-    @players << TicTacToe::Player.new('Bot', 'O', role[:bot])
-  end
-
-  def test_welcome_msg
-
-  end
 end
